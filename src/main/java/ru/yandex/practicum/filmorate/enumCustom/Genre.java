5beecd8aa7b650c7f47b321f19174b2646f4fcaa
ratd <dmitrii_r@list.ru>
2023-01-30 17:19:40 +0300

    Добавил enums - жанр, рейтинг, статус дружбы

---
+++ src/main/java/ru/yandex/practicum/filmorate/enumCustom/Genre.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.filmorate.enumCustom;
+
+public enum Genre {
+    COMEDY,
+    DRAMA,
+    CARTOON,
+    THRILLER,
+    DOCUMENTARY,
+    ACTION_MOVIE
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/enumCustom/StatusFriendship.java
@@ -0,0 +1,5 @@
+package ru.yandex.practicum.filmorate.enumCustom;
+
+public enum StatusFriendship {
+    CONFIRMED, NOT_CONFIRMED
+}
