e541e0b19119219de98777691bde0e875bbb41b9
ratd <dmitrii_r@list.ru>
2023-02-17 17:56:34 +0300

    переименовал пакет сервис 1

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.service.dbService.UserDbService;
+import ru.yandex.practicum.filmorate.service.serviceInterface.FilmServiceable;
+import ru.yandex.practicum.filmorate.service.serviceInterface.UserServiceable;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmorateApplicationTests {
 
-	private final UserDbService userDbService;
+	private final UserServiceable userServiceable;
+	private final FilmServiceable filmServiceable;
 
 	@Test
 	public void getAllUsers() {
-		userDbService.get();
+		userServiceable.get();
 	}
 }
